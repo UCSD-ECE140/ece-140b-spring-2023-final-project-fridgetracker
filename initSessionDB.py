9d0d9ed18e1cc0291af1a9dc92d3ad9340fa642e
adunkr <adunker@ucsd.edu>
2023-06-06 15:14:36 -0700

    adding init session and user python scripts

---
+++ initSessionDB.py
@@ -0,0 +1,35 @@
+import mysql.connector as mysql
+import os
+from dotenv import load_dotenv
+
+
+# Read Database connection variables
+load_dotenv("credentials.env")
+
+db_host = os.environ['MYSQL_HOST']
+db_user = os.environ['MYSQL_USER']
+db_pass = os.environ['MYSQL_PASSWORD']
+db_name = os.environ['MYSQL_DATABASE']
+
+
+# Connect to the db and create a cursor object
+db = mysql.connect(user=db_user, password=db_pass, host=db_host)
+cursor = db.cursor()
+
+
+cursor.execute("CREATE DATABASE if not exists %s" % db_name)
+cursor.execute("USE %s" % db_name)
+
+
+cursor.execute("drop table if exists Sessions;")
+try:
+   cursor.execute("""
+   CREATE TABLE Sessions (
+       sessionId VARCHAR(32) NOT NULL PRIMARY KEY,
+       username VARCHAR(30) NOT NULL,
+       timeCreated datetime
+    );
+    """)
+   db.commit()
+except RuntimeError as err:
+   print("runtime error: {0}".format(err))
---
+++ initUserDB.py
@@ -0,0 +1,36 @@
+import mysql.connector as mysql
+import os
+from dotenv import load_dotenv
+
+
+# Read Database connection variables
+load_dotenv("credentials.env")
+
+db_host = os.environ['MYSQL_HOST']
+db_user = os.environ['MYSQL_USER']
+db_pass = os.environ['MYSQL_PASSWORD']
+db_name = os.environ['MYSQL_DATABASE']
+
+
+# Connect to the db and create a cursor object
+db = mysql.connect(user=db_user, password=db_pass, host=db_host)
+cursor = db.cursor()
+
+
+cursor.execute("CREATE DATABASE if not exists %s" % db_name)
+cursor.execute("USE %s" % db_name)
+
+
+cursor.execute("drop table if exists Users;")
+try:
+   cursor.execute("""
+   CREATE TABLE Users (
+       firstname VARCHAR(20) NOT NULL,
+       lastname VARCHAR(30) NULL,
+       username VARCHAR(30) NOT NULL PRIMARY KEY,
+       password VARCHAR(30) NOT NULL
+    );
+    """)
+   db.commit()
+except RuntimeError as err:
+   print("runtime error: {0}".format(err))
